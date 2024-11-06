4ff6a8f71d460a5ff740e975542a4f6cb2950a2f
Anuskawebb <anuskasarkarofficial@gmail.com>
2024-11-06 10:50:34 +0530

    added new project

---
+++ imageStorer/imageStorer/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ImageStorageForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLast = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnDeleteImage = New System.Windows.Forms.Button()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.btnAddImage = New System.Windows.Forms.Button()
+        Me.btnStoreImage = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnLast
+        '
+        Me.btnLast.Location = New System.Drawing.Point(1033, 636)
+        Me.btnLast.Name = "btnLast"
+        Me.btnLast.Size = New System.Drawing.Size(126, 37)
+        Me.btnLast.TabIndex = 15
+        Me.btnLast.Text = "Last"
+        Me.btnLast.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(273, 469)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(107, 47)
+        Me.btnPrint.TabIndex = 14
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnDeleteImage
+        '
+        Me.btnDeleteImage.Location = New System.Drawing.Point(273, 327)
+        Me.btnDeleteImage.Name = "btnDeleteImage"
+        Me.btnDeleteImage.Size = New System.Drawing.Size(107, 46)
+        Me.btnDeleteImage.TabIndex = 13
+        Me.btnDeleteImage.Text = "Delete"
+        Me.btnDeleteImage.UseVisualStyleBackColor = True
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(273, 400)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(107, 49)
+        Me.btnUpload.TabIndex = 12
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(547, 636)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(138, 37)
+        Me.btnFirst.TabIndex = 11
+        Me.btnFirst.Text = "First"
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(273, 257)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(107, 40)
+        Me.btnPrevious.TabIndex = 10
+        Me.btnPrevious.Text = "Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(273, 196)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(107, 43)
+        Me.btnNext.TabIndex = 9
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.PictureBox1.Location = New System.Drawing.Point(493, 50)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(704, 563)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(63, 422)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 15
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'btnAddImage
+        '
+        Me.btnAddImage.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnAddImage.Location = New System.Drawing.Point(380, 69)
+        Me.btnAddImage.Name = "btnAddImage"
+        Me.btnAddImage.Size = New System.Drawing.Size(92, 50)
+        Me.btnAddImage.TabIndex = 16
+        Me.btnAddImage.Text = "Add"
+        Me.btnAddImage.UseVisualStyleBackColor = True
+        '
+        'btnStoreImage
+        '
+        Me.btnStoreImage.Location = New System.Drawing.Point(380, 574)
+        Me.btnStoreImage.Name = "btnStoreImage"
+        Me.btnStoreImage.Size = New System.Drawing.Size(92, 39)
+        Me.btnStoreImage.TabIndex = 17
+        Me.btnStoreImage.Text = "Store"
+        Me.btnStoreImage.UseVisualStyleBackColor = True
+        '
+        'ImageStorageForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1304, 742)
+        Me.Controls.Add(Me.btnStoreImage)
+        Me.Controls.Add(Me.btnAddImage)
+        Me.Controls.Add(Me.btnLast)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnDeleteImage)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.btnFirst)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "ImageStorageForm"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnLast As System.Windows.Forms.Button
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents btnDeleteImage As System.Windows.Forms.Button
+    Friend WithEvents btnUpload As System.Windows.Forms.Button
+    Friend WithEvents btnFirst As System.Windows.Forms.Button
+    Friend WithEvents btnPrevious As System.Windows.Forms.Button
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents btnAddImage As System.Windows.Forms.Button
+    Friend WithEvents btnStoreImage As System.Windows.Forms.Button
+
+End Class
---
+++ imageStorer/imageStorer/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Interop.Excel
+
+Public Class ImageStorageForm
+    Dim excelApp As Application
+    Dim workBook As Workbook
+    Dim workSheet As Worksheet
+    Dim currentImageIndex As Integer = -1
+    Dim imageList As New List(Of String)
+    Dim excelFilePath As String = "C:\Users\anusk\OneDrive\Desktop\,NET projects\Image.xlsx" ' Ensure this path is valid
+
+    Private Sub ImageStorageForm_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        InitializeExcel()
+        LoadImagePathsFromExcel() ' Load existing image paths from Excel on startup
+    End Sub
+
+    Private Sub LoadImagePathsFromExcel()
+        Try
+            ' Open the existing Excel file to read saved image paths
+            If File.Exists(excelFilePath) Then
+                workBook = excelApp.Workbooks.Open(excelFilePath)
+                workSheet = CType(workBook.Worksheets(1), Worksheet)
+
+                ' Iterate over rows in the Excel file starting from the second row (to skip headers)
+                Dim rowIndex As Integer = 2
+                While workSheet.Cells(rowIndex, 1).Value IsNot Nothing
+                    Dim imagePath As String = workSheet.Cells(rowIndex, 1).Value.ToString()
+                    imageList.Add(imagePath) ' Add each image path to imageList
+                    rowIndex += 1
+                End While
+            End If
+        Catch ex As Exception
+            ' Handle errors (e.g., Excel file not found or other issues)
+        End Try
+    End Sub
+
+
+
+    Private Sub InitializeExcel()
+        Try
+            excelApp = New Application
+            excelApp.Visible = True ' Set Excel to visible for debugging
+            workBook = excelApp.Workbooks.Add()
+            workSheet = CType(workBook.Worksheets(1), Worksheet)
+            workSheet.Cells(1, 1) = "ImagePath" ' Header for the image paths
+        Catch ex As Exception
+            ' Handle any errors without showing message boxes
+        End Try
+    End Sub
+
+    Private Sub btnAddImage_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnAddImage.Click
+        Using openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+            openFileDialog.Title = "Select an Image"
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                Dim imagePath As String = openFileDialog.FileName
+                DisplayImage(imagePath)
+            End If
+        End Using
+    End Sub
+
+    Private Sub DisplayImage(ByVal imagePath As String)
+        If File.Exists(imagePath) Then
+            PictureBox1.Image = Image.FromFile(imagePath)
+            currentImageIndex = imageList.IndexOf(imagePath)
+            If currentImageIndex = -1 Then
+                imageList.Add(imagePath) ' Add to the list if it's a new image
+            End If
+        End If
+    End Sub
+
+    Private Sub btnStoreImage_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnStoreImage.Click
+        If currentImageIndex >= 0 Then
+            Dim imagePath As String = imageList(currentImageIndex)
+            SaveImagePathToExcel(imagePath)
+        End If
+    End Sub
+
+    Private Sub SaveImagePathToExcel(ByVal imagePath As String)
+        Try
+            ' Use row 2 for storing image paths and pictures
+            Dim rowIndex As Integer = 2
+
+            ' Find the next available column in the specified row
+            Dim colIndex As Integer = workSheet.UsedRange.Columns.Count + 1
+
+            ' Add the image path in the new column
+            workSheet.Cells(rowIndex, colIndex).Value = "Image " & colIndex - 1 & ": " & imagePath
+
+            ' Get the position of the target cell in the specified row and new column
+            Dim targetCell As Range = workSheet.Cells(rowIndex + 1, colIndex) ' Adjust row for image placement
+            Dim left As Double = targetCell.Left
+            Dim top As Double = targetCell.Top
+            Dim width As Double = targetCell.Width
+            Dim height As Double = targetCell.Height
+
+            ' Add the picture in the next column, resizing it to fit within the cell
+            Dim picture = workSheet.Shapes.AddPicture(imagePath, _
+                                                      Microsoft.Office.Core.MsoTriState.msoFalse, _
+                                                      Microsoft.Office.Core.MsoTriState.msoCTrue, _
+                                                      left, top, width, height)
+
+            ' Adjust the picture to center it within the cell if it doesn't fully fit
+            picture.LockAspectRatio = Microsoft.Office.Core.MsoTriState.msoTrue
+            If picture.Width > width Or picture.Height > height Then
+                ' Scale the picture to fit within cell dimensions
+                picture.Width = width * 0.9
+                picture.Height = height * 0.9
+                ' Center the image within the cell
+                picture.Left = left + (width - picture.Width) / 2
+                picture.Top = top + (height - picture.Height) / 2
+            End If
+
+        Catch ex As Exception
+            ' Optional: Handle exceptions if necessary
+        End Try
+    End Sub
+
+
+
+
+    Private Sub btnNext_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnNext.Click
+        If currentImageIndex < imageList.Count - 1 Then
+            currentImageIndex += 1
+            DisplayImage(imageList(currentImageIndex))
+        End If
+    End Sub
+
+    Private Sub btnPrevious_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnPrevious.Click
+        If currentImageIndex > 0 Then
+            currentImageIndex -= 1
+            DisplayImage(imageList(currentImageIndex))
+        End If
+    End Sub
+
+    Private Sub btnFirst_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnFirst.Click
+        If imageList.Count > 0 Then
+            currentImageIndex = 0
+            DisplayImage(imageList(currentImageIndex)) ' Display the first image in the list
+        Else
+            MessageBox.Show("No images found in the list.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+
+    Private Sub btnLast_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnLast.Click
+        If imageList.Count > 0 Then
+            currentImageIndex = imageList.Count - 1
+            DisplayImage(imageList(currentImageIndex))
+        End If
+    End Sub
+
+    Private Sub btnDeleteImage_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnDeleteImage.Click
+        If currentImageIndex >= 0 AndAlso currentImageIndex < imageList.Count Then
+            ' Remove the current image from the list
+            imageList.RemoveAt(currentImageIndex)
+
+            ' Adjust currentImageIndex
+            If currentImageIndex >= imageList.Count Then
+                currentImageIndex = imageList.Count - 1
+            End If
+
+            ' Display the next image or clear the PictureBox if the list is empty
+            If currentImageIndex >= 0 Then
+                DisplayImage(imageList(currentImageIndex))
+            Else
+                PictureBox1.Image = Nothing
+            End If
+        End If
+    End Sub
+
+    Private Sub btnPrintImage_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnPrint.Click
+        If PictureBox1.Image IsNot Nothing Then
+            Dim printDialog As New PrintDialog()
+            If printDialog.ShowDialog() = DialogResult.OK Then
+                ' Implement printing logic here
+            End If
+        End If
+    End Sub
+
+    Protected Overrides Sub OnFormClosing(ByVal e As FormClosingEventArgs)
+        Try
+            workBook.SaveAs(excelFilePath) ' Save the workbook at the specified path
+        Catch ex As Exception
+            ' Handle any errors without showing message boxes
+        Finally
+            ' Ensure Excel application is closed properly
+            workBook.Close(False)
+            excelApp.Quit()
+            Marshal.ReleaseComObject(workSheet)
+            Marshal.ReleaseComObject(workBook)
+            Marshal.ReleaseComObject(excelApp)
+        End Try
+    End Sub
+End Class
